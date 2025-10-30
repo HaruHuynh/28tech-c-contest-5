1f6a0a992f02aff717c82c64002a7d92e3ea61b1
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-30 15:38:27 +0700

    Problem (6)

---
+++ problem-06.c
@@ -0,0 +1,26 @@
+// Cho mảng số nguyên A[] gồm N phần tử, hãy đếm xem trong mảng A[] tồn tại bao nhiêu cặp số A[i] , A[j] với i khác j sao cho tổng của 2 phần tử này bằng số K cho trước.
+#include <stdio.h>
+int countCouple(int a[], int n, int k){
+    int count = 0;
+    for(int i = 0; i < n; i++){ 
+        for(int j = i + 1; j < n; j++){
+            if(a[i] + a[j] == k){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+    int k;
+    scanf("%d", &k);
+    printf("%d", countCouple(a, n, k));
+    return 0;
+}
