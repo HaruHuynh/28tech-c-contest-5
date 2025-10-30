5a4b8c7f9de18eea36ae575ec7382b441bde337e
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-30 12:51:38 +0700

    Problem (1)

---
+++ problem-01.c
@@ -0,0 +1,56 @@
+// Cho mảng số nguyên A[] gồm N phần tử, nhiệm vụ của bạn là đếm xem trong mảng có bao nhiêu số chẵn, bao nhiêu số lẻ, tổng các phần tử là số chẵn, tổng các phần tử là số lẻ.
+#include <stdio.h>
+
+int countEven(int a[], int n){
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] % 2 == 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+int countOdd(int a[], int n){
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] % 2 != 0){
+            count++;
+        }
+    } 
+    return count;
+}
+
+int sumEven(int a[], int n){
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] % 2 == 0){
+            sum += a[i];
+        }
+    }
+    return sum;
+}
+
+int sumOdd(int a[], int n){
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] % 2 != 0){
+            sum += a[i];
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+    printf("%d\n", countEven(a, n));
+    printf("%d\n", countOdd(a, n));
+    printf("%d\n", sumEven(a,n));
+    printf("%d\n", sumOdd(a, n));
+    return 0;
+}
