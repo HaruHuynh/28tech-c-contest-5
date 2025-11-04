125449561bd37432ddbea27c6e24a1855c81c4f1
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-11-04 23:35:26 +0700

    Review

--- problem-26.c
+++ problem-26.c
@@ -2,7 +2,7 @@
 // Bai nay chi phu hop khi du lieu 0 <= A[i] <= 10^7
 
 #include <stdio.h>
-int dem[10000001]; // mảng đánh dấu các số đã xuất hiện
+int dem[10000001]; // Vì là biến global, toàn bộ phần tử của dem được khởi tạo 0 mặc định trong C.
 int main() {
     int n;
     scanf("%d", &n);
@@ -11,27 +11,40 @@ int main() {
     for(int i = 0; i < n; i++){
         scanf("%d", &a[i]);
         // danh dau la a[i] da xuat hien
-        dem[a[i]] = 1;
-        if(a[i] > max) max = a[i];
+        dem[a[i]] = 1; // đánh dấu giá trị a[i] đã xuất hiện
+        if(a[i] > max) max = a[i]; // cập nhật giá trị lớn nhất đã gặp
     }
     int count = 0;
-    for(int i = 0; i <= max; i++){
-        if(dem[i] != 0) count++;
+    for(int i = 0; i <= max; i++){ // chỉ cần duyệt đến max
+        if(dem[i] != 0) count++; // nếu dem[i] khác 0 nghĩa là i đã xuất hiện trong mảng
     }
     printf("%d", count);
-    // int count = 0;
-    // for(int i = 0; i < n; i++){
-    //     int check = 1;
-    //     for(int j = 0; j < i; j++){
-    //         if(a[i] == a[j]){
-    //             check = 0;
-    //             break;
-    //         }
-    //     }
-    //     if(check == 1){
-    //         count++;
-    //     }
-    // }
-    // printf("%d", count);
     return 0;
+    /*
+    Khi đọc mảng:
+	•	i=0, a[0]=3  → dem[3]=1, max=3
+	•	i=1, a[1]=7  → dem[7]=1, max=7
+	•	i=2, a[2]=3  → dem[3]=1 (đã là 1 rồi), max=7
+	•	i=3, a[3]=0  → dem[0]=1, max=7
+	•	i=4, a[4]=10 → dem[10]=1, max=10
+	•	i=5, a[5]=7  → dem[7]=1, max=10
+	•	i=6, a[6]=7  → dem[7]=1, max=10
+	•	i=7, a[7]=2  → dem[2]=1, max=10
+
+    */
 }
+
+// int count = 0;
+// for(int i = 0; i < n; i++){
+//     int check = 1;
+//     for(int j = 0; j < i; j++){
+//         if(a[i] == a[j]){
+//             check = 0;
+//             break;
+//         }
+//     }
+//     if(check == 1){
+//         count++;
+//     }
+// }
+// printf("%d", count);
